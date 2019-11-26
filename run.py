48c250cc4c717be2d1ce1d93e2c00324641e7750
chenenquan <chenenquan@qutoutiao.net>
2019-11-26 20:21:35 +0800

    [UPDATE] update run.py add sanic web app.

--- run.py
+++ run.py
@@ -2,4 +2,31 @@
 模型服务入口
 """
 
-import click
+import os
+import asyncio
+import logging.config
+
+from sanic import Sanic
+from sanic import json
+
+from src.main import main
+
+CURRENT_WORK_DIR = os.path.dirname(os.path.abspath(__file__))
+logging.config.fileConfig(os.path.join(CURRENT_WORK_DIR, "conf", "logging.conf"))
+
+
+app = Sanic("myapp")
+
+@app.route("/")
+async def index():
+    logger = logging.getLogger("sync")
+    return json({"hello": "world!"})
+
+
+@app.route("/find")
+async def find():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True, access_log=True, host="localhost", port=80)
