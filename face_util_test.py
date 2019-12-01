e02d3cf7ce29db17eb432a4066ff869c615fb715
northfun <fanbeishuang@gmail.com>
2019-12-01 22:26:04 +0800

    [FIX] wrong index in BBoxesTool

--- face_util_test.py
+++ face_util_test.py
@@ -1,9 +1,13 @@
 import asyncio
 
 from src.utils.face_util import FaceUtil
+import pandas as pd
 
 
 async def main():
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', 50)
+
     target_path = "./static/user/陈恩泉.jpeg"
     group_path = "./static/picture/123456.jpg"
     fname = "./static/user/陈恩泉-123456.jpg"
