ee4d24ba8accbaf04a406c1c1a00c9a5517ddd83
chenenquan <chenenquan@qutoutiao.net>
2019-12-01 22:21:32 +0800

    [FIX] fix logs.

--- src/handler/base.py
+++ src/handler/base.py
@@ -16,7 +16,7 @@ class Base:
         return cls.instance
 
     async def __aenter__(self):
-        logging.info(f"{self.__class__.__name__} acquire connect.")
+        logger.info(f"{self.__class__.__name__} acquire connect.")
         if self.pool is None:
             self.pool = await aiomysql.create_pool(
                 host='127.0.0.1', port=3306, user='root', 
@@ -24,4 +24,4 @@ class Base:
             logging.info(f"{self.__class__.__name__} create connect.")
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        logging.info(f"{self.__class__.__name__} release connect.")
+        logger.info(f"{self.__class__.__name__} release connect.")
